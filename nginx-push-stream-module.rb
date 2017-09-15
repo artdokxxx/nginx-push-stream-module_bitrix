8dd6c7e5837fa13a5b077890db3624a1cf0f304a
Artem Leontiev <artdokxxx@gmail.com>
2017-09-15 14:58:26 +0300

    Add brew file for nginx-push-stream-module 0.4.1

---
+++ nginx-push-stream-module.rb
@@ -0,0 +1,12 @@
+class PushStreamNginxModule < Formula
+  desc "pure stream http push technology"
+  homepage "https://github.com/wandenberg/nginx-push-stream-module"
+  url "https://github.com/wandenberg/nginx-push-stream-module/archive/0.4.1.tar.gz"
+  sha256 "553584f557a3faec73702550e7e1417cbc0021f4f98468cc83e61e9d94def5cc"
+
+  bottle :unneeded
+
+  def install
+    pkgshare.install Dir["*"]
+  end
+end
